3ef3dfb9bbcdb28dce34c886f0caae2370493390
윤서현 <seohyun2004@naver.com>
2025-05-27 07:59:53 +0900

    fix: build CI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,24 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:get/get.dart';
 import 'package:knowme_final_new/main.dart';
+import 'package:knowme_final_new/features/membership/views/profile_screen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('TestHome에 "프로필로 이동" 버튼이 보이고, 누르면 동작한다', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      GetMaterialApp(
+        home: TestHome(),
+        getPages: [GetPage(name: '/profile', page: () => ProfileScreen())],
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('프로필로 이동'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.text('프로필로 이동'));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(ProfileScreen), findsOneWidget);
   });
 }
