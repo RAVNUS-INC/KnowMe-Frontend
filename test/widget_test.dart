6a2e58be4387bd9fb3a848c00e19c11136466ecb
윤서현 <seohyun2004@naver.com>
2025-05-26 15:56:44 +0900

    충돌 오류 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,9 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:knowme_frontend/main.dart';
 
-
 void main() {
-  testWidgets('TestHome에 "프로필로 이동" 버튼이 보이고, 누르면 동작한다', (WidgetTester tester) async {
+  testWidgets('TestHome에 "프로필로 이동" 버튼이 보이고, 누르면 동작한다',
+      (WidgetTester tester) async {
     await tester.pumpWidget(
       GetMaterialApp(
         home: TestHome(),
