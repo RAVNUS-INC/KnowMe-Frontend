492ff31bcfc10a2bc91d39dfb8e9e131351d8693
윤서현 <seohyun2004@naver.com>
2025-05-29 00:43:59 +0900

    chore: only include lib, assets, test and essentials

--- android/app/src/main/kotlin/com/example/knowme_final_new/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.knowme_final_new
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/test_kotlin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.test_kotlin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
