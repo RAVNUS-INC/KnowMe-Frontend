114b950e0a35e0d0cc04011df513320084089b90
윤서현 <seohyun2004@naver.com>
2025-06-05 01:26:32 +0900

    userid: api 연동

--- lib/features/membership/models/profile_model.dart
+++ lib/features/membership/models/profile_model.dart
@@ -1,26 +1,56 @@
-class UserProfile {
+class ProfileModel {
+  final int id;
+  final String loginId;
   final String name;
-  final String job;
-  final String intro;
-  final String university;
   final String email;
+  final String phone;
 
-  UserProfile({
-    required this.name,
-    required this.job,
-    required this.intro,
-    required this.university,
-    required this.email,
+  final String? job;
+  final String? intro;
+  final String? university;
+
+  ProfileModel({
+    this.id = 0,
+    this.loginId = '',
+    this.name = '',
+    this.email = '',
+    this.phone = '',
+    this.job,
+    this.intro,
+    this.university,
   });
+
+  factory ProfileModel.fromJson(Map<String, dynamic> json) {
+    return ProfileModel(
+      id: json['id'] ?? 0,
+      loginId: json['loginId'] ?? '',
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      phone: json['phone'] ?? '',
+      job: json['job'],
+      intro: json['intro'],
+      university: json['university'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'loginId': loginId,
+      'name': name,
+      'email': email,
+      'phone': phone,
+    };
+  }
 }
 
-// 더미 프로필 데이터
-final UserProfile dummyProfile = UserProfile(
+// 더미 프로필 데이터 (테스트용)
+final ProfileModel dummyProfile = ProfileModel(
+  id: 0,
+  loginId: 'dummyLoginId',
   name: '이한양',
-  job: '프론트엔드 개발자',
-  intro: '끊임없이 배우고 도전하는\n프론트엔드 개발자',
-  university: '한양대학교 ERICA\nICT융합학부 재학',
   email: 'adc1234@knowme.kr',
+  phone: '010-1234-5678',
 );
 
 // 메뉴 항목
